03aa979180393e106ed746bbf2c841b329323292
Valentin Valls <valentin.valls@esrf.fr>
2019-04-01 15:13:58 +0200

    Clean up cython code

--- trainingproject/cpolynom.pyx
+++ trainingproject/cpolynom.pyx
@@ -5,17 +5,8 @@
 __authors__ = ["Pierre Knobel", "Jerome Kieffer", "Pierre Palero",
                "Henri Payno", "Armando Sole", "Valentin Valls",
                "Thomas Vincent"]
-__date__ = "27/03/2019"
+__date__ = "01/04/2019"
 __license__ = "MIT"
-
-
-def sqrt(x):
-    """Return the square root of x
-
-    :param float x: Input value
-    :rtype: float
-    """
-    return x**0.5
 
 
 def polynom(a, b, c):
@@ -31,20 +22,11 @@ def polynom(a, b, c):
     if a == 0:
         # Not a polynom
         raise ValueError("Not a quadratic equation (a==0)")
-    delta = pow2(b) - 4.0 * a * c
+    delta = (b**2.0) - 4.0 * a * c
     solutions = []
     if delta > 0:
-        solutions.append((-b + sqrt(delta)) / (2.0 * a))
-        solutions.append((-b - sqrt(delta)) / (2.0 * a))
+        solutions.append((-b + (delta**0.5)) / (2.0 * a))
+        solutions.append((-b - (delta**0.5)) / (2.0 * a))
     elif delta == 0:
-        solutions.append(-b/(2.0*a))
+        solutions.append(-b / (2.0 * a))
     return solutions
-
-
-def pow2(x):
-    """Return the square of x
-    :param float x: input value
-    :rtype: float
-    """
-    return x*x
-
